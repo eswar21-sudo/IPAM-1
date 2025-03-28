35281fc84776c185909689135cff15cc506dff28
eswar21-sudo <eswarkumar5475@gmail.com>
2025-03-28 13:44:44 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "description" {
+  description = "Description for IPAM"
+  type        = string
+}
+
+variable "operating_regions" {
+  description = "List of AWS regions for IPAM"
+  type        = list(string)
+}
+
+variable "cidr_pools" {
+  description = "List of CIDR pools for IPAM"
+  type = list(object({
+    region                 = string
+    description            = string
+    cidr                   = string
+    auto_import            = bool
+    default_netmask_length = number
+  }))
+}
