2b633255785a8a77a8636d6e6d88d57595081c29
eswar21-sudo <eswarkumar5475@gmail.com>
2025-03-28 13:49:45 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc_ipam" "this" {
   dynamic "operating_regions" {
     for_each = var.operating_regions 
 content {
-      region_name = region
+      region_name = us-east-1
     }
   }
 }
